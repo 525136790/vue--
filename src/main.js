423b2c92a019a904e31ad96a1741ffaaf5b24cf4
525136790 <56470117+525136790@users.noreply.github.com>
2020-03-18 13:45:42 +0800

    2020-03-118
    1.ele-ui 实现导航菜单
    2.通过axios拦截器未请求头中添加token验证
    3.发起请求，获取导航菜单的信息
    4.首页路由重定向

--- src/main.js
+++ src/main.js
@@ -13,6 +13,14 @@ import './assets/fonts/iconfont.css'
 import axios from "axios"
 // 配置请求的根路径
 axios.defaults.baseURL ='http://127.0.0.1:8888/api/private/v1/'
+//通过设置axios拦截器来添加token验证
+axios.interceptors.request.use(config=>{
+  // console.log(config)
+  // 在请求头中添加 token字段，
+  config.headers.Authorization =window.sessionStorage.getItem('token');
+  // 必须要 return config
+  return config
+})
 Vue.prototype.$http = axios;
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,23 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/login'
 import  Home from '../components/home'
+import Welcome from '../components/Welcome'
+import Users from '../components/user/Users'
+
 Vue.use(VueRouter)
 
 const routes = [
   {path:'/',redirect:'/login'},
   {path:'/login',component:Login},
-  {path:'/home',component:Home}
+  {
+    path:'/home',
+    component:Home,
+    redirect:'/welcome',//重定向到子级
+    children:[
+      {path:'/welcome',component:Welcome},
+      {path:'/users',component:Users}
+    ]
+  }
 ]
 
 const router = new VueRouter({
