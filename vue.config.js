51ce22f089b1cd314f1f46ea45702cd52b6bcf38
525136790 <525136790@qq.com>
2020-03-20 15:24:25 +0800

    Revert "2020-03-118"
    This reverts commit 423b2c92a019a904e31ad96a1741ffaaf5b24cf4.

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,32 @@
 module.exports = {
   // publicpath: process.env.NODE_ENV === 'production' ? '192.0.0.1:8808' : '192.0.0.1:8808',
   devServer: {
-    port: 8083,
+    port: 8000,
     open: true,
+    Proxy: {
+      '/php': {
+        target: 'http://localhost/php/interface/',
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          '^/php': ''
+        }
+      },
+      '/douban': {
+        target: 'https://douban.uieee.com/v2',
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          '^/douban': ''
+        }
+      },
+      '/elem': {
+        target: 'https://elm.cangdu.org',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/elem': ''
+        }
+      }
     }
+  }
 }
