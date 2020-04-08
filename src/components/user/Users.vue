fc695581c281d5d3743fa3ddcdbf7b73ab392d43
baixuejing <525136790@qq.com>
2020-04-08 11:02:50 +0800

    2020-04-08
    分页功能的实现，及搜索功能的实现

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -6,22 +6,104 @@
             <el-breadcrumb-item>用户管理</el-breadcrumb-item>
             <el-breadcrumb-item>用户列表</el-breadcrumb-item>
         </el-breadcrumb>
+        <!-- 卡片视图区 -->
         <el-card class="box-card">
+            <!-- 搜索与添加用户区 -->
             <el-row :gutter="20">
-                <el-col :span='8'>
-                    <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
-                        <el-button slot="append" icon="el-icon-search"></el-button>
+                <el-col :span="8">
+                    <el-input placeholder="请输入内容" v-model='queryInfo.query' clearable @clear='getUserList'>
+                        <el-button slot="append" icon="el-icon-search" @click='getUserList'></el-button>
                     </el-input>
                 </el-col>
-                <el-col :span='4'>
-                    <el-button type='primary'>添加用户</el-button>
+                <el-col :span="4">
+                    <el-button type="primary">添加用户</el-button>
                 </el-col>
             </el-row>
+            <!-- 用户显示列表 -->
+            <el-table :data="userList" border stripe>
+                <el-table-column type="index" label="#"></el-table-column>
+                <el-table-column prop="username" label="姓名"></el-table-column>
+                <el-table-column prop="email" label="邮箱"></el-table-column>
+                <el-table-column prop="mobile" label="电话"></el-table-column>
+                <el-table-column prop="role_name" label="角色"></el-table-column>
+                <el-table-column label="状态">
+                    <template slot-scope="scope">
+                        <el-switch v-model="scope.row.mg_state" @change="userStateChange(scope.row)"></el-switch>
+                    </template>
+                </el-table-column>
+                <el-table-column label="操作" width="180px">
+                    <template slot-scope="scope">
+                        <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+                        <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+                        <el-tooltip effect="dark" content="分配角色" :enterable="false" placement="top">
+                            <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+                        </el-tooltip>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <!-- 分页区域 -->
+            <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="queryInfo.pagenum"
+                :page-sizes="[1,2,5,10]"
+                :page-size="queryInfo.pagesize"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="total">
+            </el-pagination>
         </el-card>
     </div>
 </template>
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            //获取用户列表的参数对象
+            queryInfo: {
+                query: "",   //搜索关键字
+                pagenum: 1, //获取哪一页
+                pagesize: 2  //一页显示的数量
+            },
+            userList: [],
+            total: 0
+        };
+    },
+    created() {
+        this.getUserList();
+    },
+    methods: {
+        async getUserList() {
+            const { data: res } = await this.$http.get("users", {
+                params: this.queryInfo
+            });
+            if (res.meta.status !== 200)
+                return this.$message("获取用户列表失败！");
+            this.userList = res.data.users;
+            this.total = res.data.total;
+        },
+        // 监听 pagesize改变后的函数
+        handleSizeChange(newSize){
+            console.log(newSize)
+            this.queryInfo.pagesize = newSize;
+            this.getUserList()
+        },
+        //监听 页码值改变的函数
+        handleCurrentChange(newPage){
+            console.log(newPage)
+            this.queryInfo.pagenum = newPage;
+            this.getUserList()
+        },
+        // 改变用户状态并调用接口同步到服务器中
+        async userStateChange(userinfo){
+            const {data:res} = await this.$http.put(`users/${userinfo.id}/state/${userinfo.mg_state}`)
+            if(res.meta.status !== 200){
+                userinfo.mg_state = !userinfo.mg_state;
+                return this.$message.error('更新用户状态失败')
+            }
+            this.$message.success('更新用户状态成功')
+        },
+    }
+};
 </script>
 
 <style lang="less" scoped>
